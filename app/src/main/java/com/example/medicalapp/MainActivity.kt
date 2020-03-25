d413e5358259f56950921e9bfd7f2c2bdd3bf97d
Vaasudev Guduguntla <vaasudevguduguntla@Vaasudevs-MacBook-Pro.local>
2020-03-24 20:45:00 -0400

    working navigation

--- app/src/main/java/com/example/medicalapp/MainActivity.kt
+++ app/src/main/java/com/example/medicalapp/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.medicalapp
 
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isNotEmpty
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.medicalapp.fragments.MyNetworkFragment
@@ -14,14 +16,19 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
-    var bottomNavigation: BottomNavigationView? = null
+    lateinit var bottomNavigation: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener)
+        openFragment(MyProfileFragment.newInstance("", ""))
+
+
     }
 
     fun openFragment(fragment: Fragment) {
+        Log.d("GOT HERE", "HI")
         val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.container, fragment)
         transaction.addToBackStack(null)
@@ -31,17 +38,21 @@ class MainActivity : AppCompatActivity() {
     var navigationItemSelectedListener: BottomNavigationView.OnNavigationItemSelectedListener =
         object : BottomNavigationView.OnNavigationItemSelectedListener {
             override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                Log.d("GOT HERE", "HI")
                 when (item.getItemId()) {
                     R.id.fragment_my_profile -> {
                         openFragment(MyProfileFragment.newInstance("", ""))
+
                         return true
                     }
                     R.id.fragment_my_patients -> {
                         openFragment(MyPatientsFragment.newInstance("", ""))
+
                         return true
                     }
                     R.id.fragment_my_network -> {
                         openFragment(MyNetworkFragment.newInstance("", ""))
+
                         return true
                     }
                 }
