5eca00dc81d7f41d1ea62e7c461d0fc828b17eb1
Vaasudev Guduguntla <vaasudevguduguntla@Vaasudevs-MacBook-Pro.local>
2020-03-28 16:26:04 -0400

    yo

--- app/src/main/java/com/example/medicalapp/fragments/MyPatientsFragment.java
+++ app/src/main/java/com/example/medicalapp/fragments/MyPatientsFragment.java
@@ -1,12 +1,10 @@
 package com.example.medicalapp.fragments;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
