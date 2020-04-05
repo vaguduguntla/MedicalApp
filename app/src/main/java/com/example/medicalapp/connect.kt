b06285dc89b782a70719f4cdb1960cb6592bb7e6
zebklein <zebklein@umich.edu>
2020-04-05 18:13:57 -0400

    moving to java

--- app/src/main/java/com/example/medicalapp/connect.kt
+++ app/src/main/java/com/example/medicalapp/connect.kt
@@ -1,40 +1,58 @@
 package com.example.medicalapp
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
 
 class Connect : ViewModel() {
 
     // The internal MutableLiveData String that stores the status of the most recent request
     private val _response = MutableLiveData<String>()
 
+    private lateinit var query: String
+
+    private val BASE_URL = "https://amh3vre134.execute-api.us-east-2.amazonaws.com/default/"
+
     // The external immutable LiveData for the request status String
     val response: LiveData<String>
-        get() = _response
+    get() = _response
 
-    /**
+    interface MedApiService {
+        @GET("foo?" + query)
+        fun getProperties():
+                Call<String>
+    }
+
+    object MedApi {
+        val retrofitService : MedApiService by lazy {
+            retrofit.create(MedApiService::class.java)
+        }
+    }
+
+     /**
      * Call getRecords() on init so we can display status immediately.
      */
     init {
         getRecords()
     }
 
-    /**
-     * Sets the value of the status LiveData to the Med API status.
-     */
-    private fun getRecords() {
+    fun setQuery(q: String) {
+        query = q
+    }
+
+    fun getRecords() {
         MedApi.retrofitService.getProperties().enqueue( object: Callback<String> {
             override fun onFailure(call: Call<String>, t: Throwable) {
                 _response.value = "Failure: " + t.message
             }
             override fun onResponse(call: Call<String>, response: Response<String>) {
-                Log.d("response", "responding")
-                _response.value = response.body()
+            _response.value = response.body()
             }
         })
     }
--- app/src/main/java/com/example/medicalapp/retrofit_test.kt
+++ app/src/main/java/com/example/medicalapp/retrofit_test.kt
@@ -1,3 +1,4 @@
+
 package com.example.medicalapp
 
 import retrofit2.Call
@@ -7,19 +8,19 @@ import retrofit2.http.GET
 
 private const val BASE_URL = "https://amh3vre134.execute-api.us-east-2.amazonaws.com/default/"
 
-private val retrofit = Retrofit.Builder()
+val retrofit = Retrofit.Builder()
     .addConverterFactory(ScalarsConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
-
+/*
 interface MedApiService {
     @GET("foo?users")
     fun getProperties():
             Call<String>
 }
-
 object MedApi {
     val retrofitService : MedApiService by lazy {
         retrofit.create(MedApiService::class.java)
     }
 }
+*/
